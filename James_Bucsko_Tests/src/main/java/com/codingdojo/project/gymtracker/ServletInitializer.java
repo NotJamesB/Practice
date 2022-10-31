f63cf630d16ad1bd1ece8c2fb27c604d88d3ac25
James Bucsko <97855508+NotJamesB@users.noreply.github.com>
2022-10-31 13:40:12 -0700

    Add files via upload

---
+++ James_Bucsko_Tests/src/main/java/com/codingdojo/project/gymtracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.codingdojo.project.gymtracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JamesBucskoTestsApplication.class);
+	}
+
+}
---
+++ James_Bucsko_Tests/src/test/java/com/codingdojo/project/gymtracker/JamesBucskoTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.project.gymtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamesBucskoTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
